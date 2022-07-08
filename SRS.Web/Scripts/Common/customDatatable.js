8f58ed4037f455b05d6306274272ed5a4f5e4142
a.hlova <a.hlova@abtollc.com>
2022-07-09 02:39:01 +0300

    fix for tables

--- SRS.Web/Scripts/Common/customDatatable.js
+++ SRS.Web/Scripts/Common/customDatatable.js
@@ -5,7 +5,10 @@ $(document).ready(function () {
             dom: 'lBfrtip',
             buttons: [
                 'csv', 'excel'
-            ]
+            ],
+            language: {
+                url: '//cdn.datatables.net/plug-ins/1.12.1/i18n/uk.json'
+            }
         }
     );
 });
