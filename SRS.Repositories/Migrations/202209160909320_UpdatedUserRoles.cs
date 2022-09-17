6f01a22a9159215c7acf2d8c9702ba68c0888a79
a.hlova <a.hlova@abtollc.com>
2022-09-17 03:17:47 +0300

    fixed migration

--- SRS.Repositories/Migrations/202209160909320_UpdatedUserRoles.cs
+++ SRS.Repositories/Migrations/202209160909320_UpdatedUserRoles.cs
@@ -9,6 +9,16 @@
             Sql(@"ALTER TABLE AspNetUserRoles
                 DROP CONSTRAINT [FK_dbo.AspNetUserRoles_dbo.AspNetRoles_RoleId];");
 
+            Sql(@"update AspNetUserRoles
+                set RoleId = 
+                (case when AspNetRoles.Name=N'Superadmin' then '1' 
+                when AspNetRoles.Name=N'Адміністрація ректорату' then '2'
+                when AspNetRoles.Name=N'Адміністрація деканату' then '3'
+                when AspNetRoles.Name=N'Керівник кафедри' then '4' 
+                when AspNetRoles.Name=N'Працівник' then '5'
+                else RoleId end)
+                FROM AspNetUserRoles JOIN AspNetRoles ON AspNetUserRoles.RoleId = AspNetRoles.Id;");
+
             Sql(@"delete from AspNetRoles;");
 
             Sql(@"Insert into AspNetRoles(Id, Name) values
@@ -18,19 +28,9 @@
                 ('4', N'Керівник кафедри'),
                 ('5', N'Працівник');");
 
-            Sql(@"update AspNetUserRoles
-                set RoleId = 
-                (case when AspNetRoles.Name=N'Superadmin' then '1' 
-                when AspNetRoles.Name=N'Адміністрація ректорату' then '2'
-                when AspNetRoles.Name=N'Адміністрація деканату' then '3'
-                when AspNetRoles.Name=N'Керівник кафедри' then '4' 
-                when AspNetRoles.Name=N'Працівник' then '5'
-                else Id end)
-                FROM AspNetUserRoles JOIN AspNetRoles ON AspNetUserRoles.RoleId = AspNetRoles.Id;");
-
             Sql(@"ALTER TABLE AspNetUserRoles
-                ADD CONSTRAINT[FK_dbo.AspNetUserRoles_dbo.AspNetRoles_RoleId]
-                FOREIGN KEY(RoleId) REFERENCES AspNetRoles(Id); ");
+                ADD CONSTRAINT [FK_dbo.AspNetUserRoles_dbo.AspNetRoles_RoleId]
+                FOREIGN KEY(RoleId) REFERENCES AspNetRoles(Id);");
         }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Critical Code Smell", "S1186:Methods should not be empty", Justification = "No need to revert")]
