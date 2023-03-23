20f7a2b92191dd0215428940e5b67bd7afa3da41
DESKTOP-GP66IE9\onisk <onisko417@gmail.com>
2023-03-23 22:34:53 +0200

    Add pages for subparagraph of "Cathedra"

--- SRS.Services/Models/CathedraModel.cs
+++ SRS.Services/Models/CathedraModel.cs
@@ -1,4 +1,6 @@
-﻿using SRS.Services.Models.BaseModels;
+﻿using System.Collections.Generic;
+using SRS.Domain.Entities;
+using SRS.Services.Models.BaseModels;
 
 namespace SRS.Services.Models
 {
@@ -9,5 +11,9 @@ namespace SRS.Services.Models
         public string GenitiveCase { get; set; }
 
         public int? FacultyId { get; set; }
+
+        public virtual Faculty Faculty { get; set; }
+
+        public IList<ThemeOfScientificWork> ThemeOfScientificWork { get; set; }
     }
 }
---
+++ SRS.Web/Models/Shared/FacultyFilterViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace SRS.Web.Models.Shared
+{
+    public class FacultyFilterViewModel : BaseFilterViewModel
+    {
+        public int? FacultyId { get; set; }
+    }
+}
