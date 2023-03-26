4572aef7fdfb87341a55f578d8b59cc58009cf30
DESKTOP-SUIDTMC\Borys <Boryssheyka@gmail.com>
2023-03-26 17:13:25 +0300

    I added analytics button to navigation bar

---
+++ SRS.Web/Controllers/AnalyticsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Antlr.Runtime.Misc;
+using AutoMapper;
+using PagedList;
+using SRS.Services.Interfaces;
+using SRS.Services.Models;
+using SRS.Services.Models.Constants;
+using SRS.Services.Models.FilterModels;
+using SRS.Web.Models.Shared;
+
+namespace SRS.Web.Controllers
+{
+    public class AnalyticsController : Controller
+    {
+        private readonly IBaseCrudService<FacultyModel> _facultiesCrudService;
+        private readonly IFacultyService _facultiesService;
+        private readonly IMapper _mapper;
+
+        public AnalyticsController(
+            IBaseCrudService<FacultyModel> facultiesCrudService,
+            IFacultyService facultiesService,
+            IMapper mapper)
+        {
+            _facultiesCrudService = facultiesCrudService;
+            _facultiesService = facultiesService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
